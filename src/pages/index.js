93c463a969e4fa271cefa6ca471e95b2f6df62d9
peleg <pelegshefi@hotmail.com>
2022-06-12 22:20:27 +0300

    fixed login flash, added logout button

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,6 @@ export {default as Create} from './create/Create';
 export {default as FieldDetails} from './fieldDetails/FieldDetails';
 export {default as Home} from './home/Home';
 export {default as NotFound} from './notFound/NotFound';
-export {default as Login} from './login/Login'
+export {default as Login} from './login/Login';
+export {default as Loading} from './loading/Loading';
+
---
+++ src/pages/loading/Loading.jsx
@@ -0,0 +1,12 @@
+
+const Loading = () => {
+
+    return (
+        <div>
+           
+
+        </div>
+    );
+}
+
+export default Loading;
